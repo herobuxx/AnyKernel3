41aa38bf9a671a7c15c5c47d1b7d3e5e46e08a22
HeroBuxx <53365858+herobuxx@users.noreply.github.com>
2019-09-01 17:10:57 +0700

    AK3: Rename
    Verified? Yes! why not? It maede to edit file easier!

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=PressFKernel by HeroBuxx
+kernel.string=RiddimKernel by HeroBuxx
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
